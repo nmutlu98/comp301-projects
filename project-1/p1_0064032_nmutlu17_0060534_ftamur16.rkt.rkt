81ca5229080ab57dfedc6069e139f5948cfe5442
ftamur <ftamur16@ku.edu.tr>
2020-10-28 18:35:01 +0300

    Part A, C and D are done.

--- project-1/p1_0064032_nmutlu17_0060534_ftamur16.rkt.rkt
+++ project-1/p1_0064032_nmutlu17_0060534_ftamur16.rkt.rkt
@@ -1,13 +1,26 @@
 (module project1 mzscheme
+  
   ;;;;;;;;;;;; Comp 301 Project 1 ;;;;;;;;;;;;;;;;
   ;;; Add group members below
-  ;;; Alpay Sabuncuoğlu, asabuncuoglu13, 0011221
-  ;;; Gül Sena Altıntaş, galtintas17, 0011222
-  ;;; save your file in the format: p1_0011221_asabuncuoglu13_00112222_galtintas17.rkt
+
+  ;;; Necla Mutlu, nmutlu17, 0064032
+  ;;; Firat Tamur, ftamur16, 0060534
+  
+  ;;; save your file in the format: p1_0064032_nmutlu17_0060534_ftamur16.rkt
+  
   ;;;;;;;;;;;;;;;;;;; PROBLEMS ;;;;;;;;;;;;;;;;;;;;
   ;; PROJECT 1 Part A | Write your answer below here as a comment
-  ;
-  ;
+  ; 
+  ; First Grammar Definition:
+  ; Define even numbers as list of 'even and 'two.
+
+  ; evens ::= ('even) | (evens . 'two)
+  
+  ; [0]   = ('even)
+  ; [n+1] = ([n] . 'two)
+
+  ; 
+  ; Second Grammar Definition:
   ;
   ;
   ;
@@ -17,13 +30,20 @@
   ;; First Representation | We added a suffix so that both first and second representations can be tested at once.
 
   (define create-a
-    ())
+    (lambda (n)
+      (if (= n 0)
+          (list 'even)
+          (append (create-a (- n 2)) '(two)))
+      )
+    )
 
   (define is-zero-a?
-    ())
+    (lambda (n) (and (eqv? (car n) 'even)) (null? (cdr n)))
+    )
 
   (define successor-a
-    ())
+    (lambda (n) (append n '(two)))
+    )
 
   ;; Second Representation | We added a -b suffix so that both Unary and BigNum can be tested at once.
   (define create-b
@@ -37,8 +57,9 @@
 
   ;; PROJECT 1 Part C | Write your answer below here as a comment
   ;
-  ;
-  ;
+  ; create    : Constructor; create a new representation.
+  ; is-zero-b?: Predicate, observer; checks conditions about representation and does not return any representation only bool values.
+  ; successor : Constructor; create a new representation from existing representation. 
   ;
   ;
   
@@ -56,17 +77,18 @@
   
   ;; PROJECT 1 Part D | Remove the comments and write test cases.
   (display "First Representation Tests\n")
-  ;(equal?? (create-a ) '()) ; should return ?
-  ;(equal?? (is-zero-a? '()) #f) ; should return #f
-  ;(equal?? (is-zero-a? '()) #t) ; should return #t
-  ;(equal?? (successor-a '()) '()) ; should return ?
+  (equal?? (create-a 2) '(even two)) ; should return '(even)
+  (equal?? (is-zero-a? '(even two)) #f) ; should return #f
+  (equal?? (is-zero-a? '(even)) #t) ; should return #t
+  (equal?? (successor-a '(even)) '(even two)) ; should return ?
   (newline)
 
   
   (display "Second Representation Tests\n")
-  ;(equal?? (create-a ) '()) ; should return ?
-  ;(equal?? (is-zero-a? '()) #f) ; should return #f
-  ;(equal?? (is-zero-a? '()) #t) ; should return #t
-  ;(equal?? (successor-a '()) '()) ; should return ?
+  ;(equal?? (create-b ) '()) ; should return ?
+  ;(equal?? (is-zero-b? '()) #f) ; should return #f
+  ;(equal?? (is-zero-b? '()) #t) ; should return #t
+  ;(equal?? (successor-b '()) '()) ; should return ?
   (newline)
+  
 )
