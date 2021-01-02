b37a4ceaf716a886dabb1a6219b17708e10e9629
nmutlu98 <53997005+nmutlu98@users.noreply.github.com>
2021-01-03 00:14:12 +0300

    Completed task 4

--- project-4/Continuation Passing Style/fibonacci.rkt
+++ project-4/Continuation Passing Style/fibonacci.rkt
@@ -1,5 +1,4 @@
 #lang racket
-
 ;;;;;;;;;;;;;;;;;;;;;;; TASK 4 ;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -7,6 +6,18 @@
 ;;
 ; i:    1  2  3  4  5  6  7  8  9  10 11 ...
 ; f(i): 1  1  2  3  5  8  13 21 34 55 ...
+
+(define fibonacci
+  (lambda (n)
+    (cond ((eq? n 1) 1)
+          ((eq? n 2) 1)
+          (else (fibonacci-c n 0 1 (lambda (next) (+ 1 next)))))))
+
+(define fibonacci-c
+  (lambda (n n1 n2 cont)
+    (if (= n 3)
+        (cont 1)
+        (fibonacci-c (- n 1) n2 (+ n1 n2) (lambda (next) (cont (+ next (+ n1 n2))))))))
 
 ;; Tests
 (display (fibonacci 4)) ; should output 3
